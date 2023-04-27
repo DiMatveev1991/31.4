8ae08711a3f38556b0260fc0fd242013cc20ffc7
DiMatveev1991 <98545906+DiMatveev1991@users.noreply.github.com>
2023-04-27 19:51:37 +0300

    31.4
    HomeWork_slider+Dom

---
+++ LifeSpot/Static/JS/testing.js
@@ -0,0 +1,28 @@
+﻿
+let myMap = new Map();
+myMap.set("Германия", "Берлин")
+myMap.set("Швеция", "Стокгольм")
+myMap.set(1, "Москва")
+
+let newArray = [];
+
+for (let result of myMap){
+    newArray.push(result);
+}
+console.log(newArray);
+
+let newArrayOfStrings = [];
+
+Array.from(myMap, ([key,value]) => newArrayOfStrings.push(`${key} - ${value}`) );
+console.log(newArrayOfStrings);
+
+const saveInput = function () {
+   
+    let currentInput = document.getElementsByTagName('input')[0].value.toLowerCase();
+
+  
+    alert('Последний ввод: ' + this.lastInput 
+        + '\n' + 'Текущий ввод: ' + currentInput);
+
+    this.lastInput = currentInput;
+}
